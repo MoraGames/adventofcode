7e78807b0727240691aed001d70956c6e9a1bd9f
MoraGames <andreamorandi2612@gmail.com>
2024-12-03 01:20:05 +0100

    (chore) Prepare files for next day's puzzle.

---
+++ y2024/day3.go
@@ -0,0 +1,15 @@
+package y2024
+
+import "github.com/MoraGames/adventofcode/utils"
+
+func D3P1() {
+	lines := utils.SplitLines(ReadFile(3))
+
+	utils.PrintSolution("D3P1()", lines)
+}
+
+func D3P2() {
+	lines := utils.SplitLines(ReadFile(3))
+
+	utils.PrintSolution("D3P2()", lines)
+}

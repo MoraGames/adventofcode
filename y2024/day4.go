8126faf8d248298ccb8d4a5ab9427dd75bcb605c
MoraGames <andreamorandi2612@gmail.com>
2024-12-03 06:36:58 +0100

    (chore) Prepare files for next day's puzzle.

---
+++ y2024/day4.go
@@ -0,0 +1,13 @@
+package y2024
+
+func D4P1() {
+	// file := ReadFile(4)
+
+	// utils.PrintSolution("D4P1()", file)
+}
+
+func D4P2() {
+	// file := ReadFile(4)
+
+	// utils.PrintSolution("D4P2()", file)
+}
--- y2024/y2024.go
+++ y2024/y2024.go
@@ -3,12 +3,14 @@ package y2024
 import "github.com/MoraGames/adventofcode/utils"
 
 func Y2024() {
-	//D1P1()
-	//D1P2()
-	//D2P1()
-	//D2P2()
-	D3P1()
-	D3P2()
+	// D1P1()
+	// D1P2()
+	// D2P1()
+	// D2P2()
+	// D3P1()
+	// D3P2()
+	D4P1()
+	D4P2()
 }
 
 const (

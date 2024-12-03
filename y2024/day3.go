a9c9cc22f2b23466f1ec1a686dcd385eff3b3f95
MoraGames <andreamorandi2612@gmail.com>
2024-12-03 01:20:05 +0100

    (feat) Adds access point for 2024 puzzle module.

--- y2024/day3.go
+++ y2024/day3.go
@@ -1,15 +1,59 @@
 package y2024
 
-import "github.com/MoraGames/adventofcode/utils"
+import (
+	"regexp"
+	"strings"
+
+	"github.com/MoraGames/adventofcode/utils"
+)
 
 func D3P1() {
-	lines := utils.SplitLines(ReadFile(3))
+	instructions := ReadFile(3)
 
-	utils.PrintSolution("D3P1()", lines)
+	re := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	matches := re.FindAllStringSubmatch(instructions, -1)
+
+	var result int
+	for _, match := range matches {
+		temp := utils.SplitCommas(match[0])
+		value1 := utils.StoI(strings.Split(temp[0], "(")[1])
+		value2 := utils.StoI(strings.Split(temp[1], ")")[0])
+
+		//fmt.Printf("match = %q\n |> values = %v * %v\n", match, value1, value2)
+
+		result += value1 * value2
+	}
+
+	utils.PrintSolution("D3P1()", result)
 }
 
 func D3P2() {
-	lines := utils.SplitLines(ReadFile(3))
+	instructions := ReadFile(3)
 
-	utils.PrintSolution("D3P2()", lines)
+	re := regexp.MustCompile(`(mul\(\d{1,3},\d{1,3}\))|don't\(\)|do\(\)`)
+	matches := re.FindAllStringSubmatch(instructions, -1)
+
+	var result int
+	var isEnabled bool = true
+	for _, match := range matches {
+		if match[0] == "don't()" {
+			isEnabled = false
+			continue
+		}
+		if match[0] == "do()" {
+			isEnabled = true
+			continue
+		}
+		if isEnabled {
+			temp := utils.SplitCommas(match[0])
+			value1 := utils.StoI(strings.Split(temp[0], "(")[1])
+			value2 := utils.StoI(strings.Split(temp[1], ")")[0])
+
+			//fmt.Printf("match = %q\n |> values = %v * %v\n", match, value1, value2)
+
+			result += value1 * value2
+		}
+	}
+
+	utils.PrintSolution("D3P2()", result)
 }
---
+++ y2024/y2024.go
@@ -0,0 +1,24 @@
+package y2024
+
+import "github.com/MoraGames/adventofcode/utils"
+
+func Y2024() {
+	//D1P1()
+	//D1P2()
+	//D2P1()
+	//D2P2()
+	D3P1()
+	D3P2()
+}
+
+const (
+	year = 2024
+)
+
+func ReadFile(day int) string {
+	return utils.ReadFile(year, day)
+}
+
+func ReadTestFile(day int) string {
+	return utils.ReadTestFile(year, day)
+}

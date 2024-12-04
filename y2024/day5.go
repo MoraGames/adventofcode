2378f8dae2ec28164063fa9f0fb4c1e14ccc595a
MoraGames <andreamorandi2612@gmail.com>
2024-12-04 08:22:28 +0100

    (chore) Prepare files for next day's puzzle.

---
+++ y2024/day5.go
@@ -0,0 +1,13 @@
+package y2024
+
+func D5P1() {
+	// input := ReadFile(5)
+
+	// utils.PrintSolution("D5P1()", input)
+}
+
+func D5P2() {
+	// input := ReadFile(5)
+
+	// utils.PrintSolution("D5P2()", input)
+}

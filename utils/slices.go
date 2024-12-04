130b86962da862b02e47ee0aa795ac1a3b49c0bd
MoraGames <andreamorandi2612@gmail.com>
2024-12-04 10:00:32 +0100

    (style) Rename parameter s in slice.

--- utils/slices.go
+++ utils/slices.go
@@ -1,23 +1,23 @@
 package utils
 
 // Replaced by:
-// slices.Insert[S ~[]E, E any](s S, i int, v ...E) S
-func AddIndex[T any](s []T, index int, value T) []T {
+// slices.Insert[S ~[]E, E any](sliceS, i int, v ...E) S
+func AddIndex[T any](slice []T, index int, value T) []T {
 	newS := make([]T, 0)
-	newS = append(newS, s[:index]...)
+	newS = append(newS, slice[:index]...)
 	newS = append(newS, value)
-	return append(newS, s[index:]...)
+	return append(newS, slice[index:]...)
 }
 
-func RemoveIndex[T any](s []T, index int) []T {
+func RemoveIndex[T any](slice []T, index int) []T {
 	newS := make([]T, 0)
-	newS = append(newS, s[:index]...)
-	return append(newS, s[index+1:]...)
+	newS = append(newS, slice[:index]...)
+	return append(newS, slice[index+1:]...)
 }
 
-func Count[T comparable](s []T, value T) int {
+func Count[T comparable](slice []T, value T) int {
 	var count int
-	for _, v := range s {
+	for _, v := range slice {
 		if v == value {
 			count++
 		}

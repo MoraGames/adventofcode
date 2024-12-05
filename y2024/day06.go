f55bbb042a7e96d9573b8fe5b86f902e96efe255
MoraGames <andreamorandi2612@gmail.com>
2024-12-05 08:44:33 +0100

    (chore) Prepare files for next day's puzzle.

---
+++ y2024/day06.go
@@ -0,0 +1,15 @@
+package y2024
+
+import "github.com/MoraGames/adventofcode/utils"
+
+func D6P1() {
+	input := ReadFile(6)
+
+	utils.PrintSolution("D6P1()", input)
+}
+
+func D6P2() {
+	input := ReadFile(6)
+
+	utils.PrintSolution("D6P2()", input)
+}

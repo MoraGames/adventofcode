4eccf5d0764650f2c68bc3b7170fa4d0771df92a
MoraGames <andreamorandi2612@gmail.com>
2024-12-07 00:47:51 +0100

    (feat) Add entry point of 2023 puzzles in main function.

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	// "github.com/MoraGames/adventofcode/y2022"
+	// "github.com/MoraGames/adventofcode/y2023"
 	"github.com/MoraGames/adventofcode/y2024"
 )
 
 func main() {
 	// y2022.Y2022()
+	// y2023.Y2023()
 	y2024.Y2024()
 }

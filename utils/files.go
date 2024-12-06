262d442e3c1749ea0eabc65d17d9dfd8be983a08
MoraGames <andreamorandi2612@gmail.com>
2024-12-06 18:44:51 +0100

    (refactor) Restructure function logic for better module extensibility.

--- utils/files.go
+++ utils/files.go
@@ -5,32 +5,47 @@ import (
 	"os"
 )
 
-var (
-	FilePathYearPrefix = "y"
-	FilePathDayPrefix  = "/day"
-	FilePathTestSuffix = "-test"
-	FilePathExtension  = ".txt"
+const (
+	defaultFilePathFormat           = "./y%04d/day%02d.txt"
+	defaultTestFilePathFormat       = "./y%04d/day%02d-test.txt"
+	defaultCustomTestFilePathFormat = "./y%04d/day%02d-customtest.txt"
+	defaultOutputFilePathFormat     = "./y%04d/outputs/day%02d-%s"
 )
 
-func ReadFile(year, day int) string {
-	file, err := os.ReadFile(fmt.Sprintf("%s%04d%s%02d%s", FilePathYearPrefix, year, FilePathDayPrefix, day, FilePathExtension))
+var (
+	FilePathFormat           = defaultFilePathFormat
+	TestFilePathFormat       = defaultTestFilePathFormat
+	CustomTestFilePathFormat = defaultCustomTestFilePathFormat
+	OutputFilePathFormat     = defaultOutputFilePathFormat
+)
+
+func Read(path string, args ...any) string {
+	file, err := os.ReadFile(fmt.Sprintf(path, args...))
 	if err != nil {
 		panic(err)
 	}
 	return string(file)
+}
+
+func ReadFile(year, day int) string {
+	return Read(defaultFilePathFormat, year, day)
 }
 
 func ReadTestFile(year, day int) string {
-	file, err := os.ReadFile(fmt.Sprintf("%s%04d%s%02d%s%s", FilePathYearPrefix, year, FilePathDayPrefix, day, FilePathTestSuffix, FilePathExtension))
-	if err != nil {
-		panic(err)
-	}
-	return string(file)
+	return Read(defaultTestFilePathFormat, year, day)
 }
 
-func WriteFile(path string, content string) {
-	err := os.WriteFile(path, []byte(content), 0644)
+func ReadCustomTestFile(year, day int) string {
+	return Read(defaultCustomTestFilePathFormat, year, day)
+}
+
+func Write(content string, path string, args ...any) {
+	err := os.WriteFile(fmt.Sprintf(path, args...), []byte(content), 0644)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func WriteOutput(content string, year, day int, filename string) {
+	Write(content, defaultOutputFilePathFormat, year, day, filename)
 }

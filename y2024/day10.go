69465e24e5b270d79571760e1779b42c6b98cc8c
MoraGames <andreamorandi2612@gmail.com>
2024-12-10 13:28:51 +0100

    (chore) Prepare files to next day's puzzle.

---
+++ y2024/day10.go
@@ -0,0 +1,15 @@
+package y2024
+
+func D10P1() {
+	day, part = 10, 1
+	input := ReadFile()
+
+	PrintSolution(input)
+}
+
+func D10P2() {
+	day, part = 10, 2
+	input := ReadFile()
+
+	PrintSolution(input)
+}
